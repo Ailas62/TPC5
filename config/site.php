1892641028e541616533a9d948d9670ca28264fb
Ailas62 <fait.morgan@gmail.com>
2014-02-25 04:51:19 +0100

    Delete site.php

--- config/site.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define('DB_SERVER', 'db516628526.db.1and1.com');
-define('DB_USERNAME', 'dbo516628526');
-define('DB_PASSWORD', '1Ncorrect');
-define('DB_DATABASE', 'db516628526');
-define('PASSWORD_SALT', 'M70Xr4oxfGW4zl3UHC9xDVRTIhPCsyPeeLgEnvglkzvXBgkb6JUEPg6BKCIILNNO');
